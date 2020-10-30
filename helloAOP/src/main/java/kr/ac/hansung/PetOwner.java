986447ded9576115aceb16ffd76e09d1ee6ca953
Jisung Jung <52397000+Jeongjibsa@users.noreply.github.com>
2020-10-30 11:28:07 +0900

    Add files via upload

---
+++ helloAOP/src/main/java/kr/ac/hansung/PetOwner.java
@@ -0,0 +1,14 @@
+package kr.ac.hansung;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class PetOwner {
+	@Autowired
+	@Qualifier(value="qf_cat")
+	private AnimalType animal;
+	
+	public void play() {
+		animal.sound();
+	}
+}
